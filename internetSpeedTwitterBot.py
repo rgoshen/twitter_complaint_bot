167e3423520e5b5ce747228bc4f2219df5727787
rgoshen <rick.goshen@gmail.com>
2020-12-24 13:45:39 -0700

    added twitter feed

--- internetSpeedTwitterBot.py
+++ internetSpeedTwitterBot.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
+
+TWITTER_EMAIL = "rickgoshen"
+TWITTER_PW = "NitL10n$#1"
+PROMISED_DOWN = 400
+PROMISED_UP = 10
 
 
 class InternetSpeedTwitterBot():
@@ -31,4 +37,30 @@ class InternetSpeedTwitterBot():
         """
         Method to send tweet to Twitter.
         """
-        pass
+        self.driver.get("https://twitter.com/login")
+
+        time.sleep(2)
+        email = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
+        password = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
+
+        email.send_keys(TWITTER_EMAIL)
+        password.send_keys(TWITTER_PW)
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        tweet_compose = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+
+        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+        tweet_compose.send_keys(tweet)
+        time.sleep(3)
+
+        tweet_button = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+
+        time.sleep(2)
+        self.driver.quit()
